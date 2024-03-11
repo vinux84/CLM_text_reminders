24179bedde9fc1bd74af4c28dc881e71d5e18f45
Vincent <vincentb0824@gmail.com>
2024-03-10 20:51:07 -0700

    Create text_reminder.py

---
+++ text_reminder.py
@@ -0,0 +1,10 @@
+Hello Brother Mashtal, just a reminder that for this coming Tuesday night’s meeting you are scheduled for the part:
+
+#5 - Kindness-Imitate Jesus
+
+Please text back to confirm that you can or cannot do the assignment.
+
+Thank you.
+
+Your Brother,
+-Vincent Benningfield-
